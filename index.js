aabf98c30f6797d7a5ff8308cbf4d2163b1943c8
mhmdbdalmnmmhmwd71@gmail.com <mhmdbdalmnmmhmwd71@gmail.com>
2026-07-13 05:09:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs=require('fs');
+const { error } = require('console');
+
+// read file
+fs.readFile('test.txt',"utf-8",(error,data)=>{
+    if(error){
+        console.error(error);
+        return;
+    }
+    console.log(data)
+
+});
+
+
+// create file or update content
+fs.writeFile("test.txt","My name is Mohamed",(err)=>{
+
+      if(err){
+        console.error(err);
+        return;
+    }
+    console.log("file created or updated")
+
+});
+
